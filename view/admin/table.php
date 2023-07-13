42b372e4beff1b375c75b92eae768b9feacbd107
Fabs90 <fabianjuliansyah89@gmail.com>
2023-07-13 19:16:20 +0700

    Menambahkan link register pada sidebar

--- view/admin/table.php
+++ view/admin/table.php
@@ -110,16 +110,14 @@ $db = new connection();
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="bi bi-wrench-adjustable mr-2"></i>
-                    <span>Utilities</span>
+                    <span>Register</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
+                        <a class="collapse-item" href="../landingPage/register.php">Register</a>
+
                     </div>
                 </div>
             </li>
